d381245c58f8c1d514c678194cd22ee23137d24e
manuelzzz <manuel.souza.017@ufrn.edu.com>
2024-09-12 09:02:33 -0300

    chore: modular route to details screen.

--- lib/src/modules/pesquisa/presenter/pesquisa_module.dart
+++ lib/src/modules/pesquisa/presenter/pesquisa_module.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:github_search/src/modules/pesquisa/domain/entities/repositorio.dart';
 import 'package:github_search/src/modules/pesquisa/domain/usecases/get_repositorios.dart';
 import 'package:github_search/src/modules/pesquisa/external/datasources/pesquisa_github_datasource_impl.dart';
 import 'package:github_search/src/modules/pesquisa/infra/repositories/pesquisa_repository_impl.dart';
+import 'package:github_search/src/modules/pesquisa/presenter/screens/detalhes_screen.dart';
 import 'package:github_search/src/modules/pesquisa/presenter/pesquisa_bloc.dart';
-import 'package:github_search/src/modules/pesquisa/presenter/pesquisa_screen.dart';
+import 'package:github_search/src/modules/pesquisa/presenter/screens/pesquisa_screen.dart';
 
 class PesquisaModule extends Module {
   @override
@@ -26,6 +28,12 @@ class PesquisaModule extends Module {
     r.child(
       Modular.initialRoute,
       child: (context) => PesquisaScreen(),
+    );
+    r.child(
+      '/detalhes',
+      child: (context) => DetalhesScreen(
+        repositorio: r.args.data as Repositorio,
+      ),
     );
   }
 }
